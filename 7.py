e6f6d56397b9a33b087bdedf3dfb2095344c39a2
RickForce <Rick@Rickforce.com>
2023-12-07 09:35:43 +0100

    6b

--- 7.py
+++ 7.py
@@ -5,38 +5,55 @@ from util import console, parse_file_as_list, time_function
 from collections import Counter
 import re
 
-
 test_file = parse_file_as_list('input/7_test.txt')
 day_file = parse_file_as_list('input/7.txt')
 
-card_rank_dict = {'A': 13, 'K': 12, 'Q': 11, 'J': 10, 'T': 9, '9': 8, '8': 7, '7': 6, '6': 5, '5': 4, '4': 3, '3': 2, '2': 1}
 
 @time_function()
 def run_a(file: list[str]):
-    hand_bet_list = get_hands_bet_dict(file)
+    card_rank_dict = {'A': 13, 'K': 12, 'Q': 11, 'J': 10, 'T': 9, '9': 8, '8': 7, '7': 6, '6': 5, '5': 4, '4': 3,
+                      '3': 2, '2': 1}
+
+    hand_bet_list = get_hands_bet_dict(file, card_rank_dict)
     for hand in hand_bet_list:
         determine_hand_rank(hand)
 
-    return sum([(index + 1) * hand['bet']  for index, hand in enumerate(sorted(hand_bet_list, key=itemgetter('hand_rank', 'hand_cards_ranked')))])
+    return sum([(index + 1) * hand['bet'] for index, hand in
+                enumerate(sorted(hand_bet_list, key=itemgetter('hand_rank', 'hand_cards_ranked')))])
 
 
 @time_function()
 def run_b(file: list[str]):
-    pass
+    card_rank_dict = {'A': 13, 'K': 12, 'Q': 11, 'T': 10, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4,
+                      '3': 3, '2': 2, 'J': 1}
+
+    hand_bet_list = get_hands_bet_dict(file, card_rank_dict)
+    for hand in hand_bet_list:
+        determine_hand_rank(hand, True)
+
+    return sum([(index + 1) * hand['bet'] for index, hand in
+                enumerate(sorted(hand_bet_list, key=itemgetter('hand_rank', 'hand_cards_ranked')))])
 
 
-def get_hands_bet_dict(file: list[str]) -> list[dict[str:any]]:
-    return [{'hand': hand,
-             'bet': int(bet),
-             'counter': Counter(hand),
+def get_hands_bet_dict(file: list[str], card_rank_dict: dict[str:int]) -> list[dict[str:any]]:
+    return [{'hand':              hand,
+             'bet':               int(bet),
+             'counter':           Counter(hand),
              'hand_cards_ranked': [card_rank_dict[card] for card in hand]
              } for hand, bet in [line.split(' ') for line in file]]
 
 
-def determine_hand_rank(hand: dict[str:any]):
+def determine_hand_rank(hand: dict[str:any], joker_style: bool = False):
+    joker_bonus = 0
+    if joker_style:
+        if hand['counter']['J'] < 5:
+            joker_bonus = hand['counter']['J']
+            hand['counter']['J'] = 0
+
     # determine highest counter
     sorted_counter = hand['counter'].most_common()
-    card_highest_count, highest_count = sorted_counter[0]
+    highest_count = sorted_counter[0][1] + joker_bonus
+
     if highest_count == 5:
         hand['hand_rank'] = 7
     elif highest_count == 4:
@@ -55,7 +72,7 @@ def determine_hand_rank(hand: dict[str:any]):
 
 if __name__ == '__main__':
     answer_a = run_a(day_file)
-    answer_b = run_b(test_file)
+    answer_b = run_b(day_file)
 
     console.print(f'solution A: {answer_a}')
     console.print(f'solution B: {answer_b}')
